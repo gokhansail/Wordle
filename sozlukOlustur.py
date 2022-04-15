8e93375560e26742346da271b8e11546bf53540f
gokhansail <48831719+gokhansail@users.noreply.github.com>
2022-04-15 22:10:49 +0300

    Altı kelimelik sözcükler filtrelendi. Bu işi yapan kod yazıldı.

---
+++ sozlukOlustur.py
@@ -0,0 +1,16 @@
+f = open("sozluk.txt", "r", encoding="cp1254")
+
+sixLetterWords = []
+
+for line in f:
+    line = line.strip('\n')
+    line = line.replace("ÄŸ", 'ğ').replace("ÅŸ", 'ş').replace("Ä°", 'İ').replace("Ä±", 'ı').replace("Ã¼", 'ü').replace("Ã¶", 'ö').replace("Ã§", 'ç')
+    line = line.upper()
+    if len(line) == 6:
+        sixLetterWords.append(line)
+
+
+with open('sozlukAlti.txt', 'w') as out:
+    for l in sixLetterWords:
+        #l = l.decode('utf8')
+        out.write('{}\n'.format(l))
