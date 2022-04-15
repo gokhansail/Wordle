8b97c33e9c8649cfce96b5a8ad16271bcc97e4a5
gokhansail <48831719+gokhansail@users.noreply.github.com>
2022-04-15 22:50:15 +0300

    tamamlandı

--- kelimeBul.py
+++ kelimeBul.py
@@ -1,6 +1,10 @@
 f = open("sozlukAlti.txt", "r", encoding="cp1254")
 
-harfList = [a,b,c]
+harfList = ['a', 'd', 'l', 'e']
+olmayanHarfList = ['p', 'r', 'm', 'k', 'ç', 'y', 'o', 'ş']
+pozisyondaOlmayanHarfList = [[0, 'e'], [1, 'a'], [2, 'l'], [3, 'd'], [4, 'a'], [5, 'a'], [1, 'l']]
+
+pozisyondaHarfList = [[0, ''], [1, ''], [2, ''], [3, ''], [4, ''], [5, '']]
 gecerliKelimeler = []
 
 def HarfIcersin(word):
@@ -11,6 +15,25 @@ def HarfIcersin(word):
     return True
 
 def HarfIcermesin(word):
+    for harf in olmayanHarfList:
+        if harf in word:
+            return False
+
+    return True
+
+def PozisyondaHarfIcersin(word):
+    for harf in pozisyondaHarfList:
+        if word[harf[0]] != harf[1]:
+            return False
+
+    return True
+
+def PozisyonKontrol(word):
+    for harf in pozisyondaOlmayanHarfList:
+        if word[harf[0]] == harf[1]:
+            return False
+
+    return True
 
 
 for kelime in f:
@@ -18,4 +41,16 @@ for kelime in f:
     if HarfIcersin(kelime) == False:
         continue
 
-    if
+    if HarfIcermesin(kelime) == False:
+        continue
+
+    #if PozisyondaHarfIcersin(kelime) == False:
+    #    continue
+
+    if PozisyonKontrol(kelime) == False:
+        continue
+
+    gecerliKelimeler.append(kelime)
+
+for kelim in gecerliKelimeler:
+    print(kelim)
